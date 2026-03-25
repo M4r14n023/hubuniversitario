70f7074254eade9244c116c2c690c2008f6b9df5
Mariano <marianolumbreras@gmail.com>
2026-03-25 20:17:22 -0300

    modo offline y ayuno act

---
+++ apps/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'agenda-v1';
+const ASSETS = [
+  './',
+  './index.html', // O el nombre de tu archivo de agenda
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
